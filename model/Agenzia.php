7ed0c479271dd56887326e6439c42ca57ae18dbd
FoldenWind <63733987+FoldenWind@users.noreply.github.com>
2020-05-01 17:47:46 +0200

    aggiunta classe Agenzia

---
+++ model/Agenzia.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class Agenzia
+{
+    private array      $list_Clienti;
+    private array      $list_AgentiImmobiliari;
+    private array      $list_Immobili;
+    private Calendario $calendario;
+
+    public function addCliente($Cliente):void{
+
+    }
+
+    public function removeCliente($Cliente):void{
+
+
+
+    }
+
+    public function addAgenteImmobiliare($AgenteImmobiliare):void{
+
+
+    }
+
+    public function removeAgenteImmobiliare($AgenteImmobiliare):void{
+
+
+    }
+
+    public function addImmobile($Immobile):void{
+
+
+    }
+
+    public function removeImmobile($Immobile):void{
+
+
+    }
+}
